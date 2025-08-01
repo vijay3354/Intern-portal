c4c29dd886bc6d22b8a4730dabcbc1aa41403afd
Vijay Gaud <vijaygoud930@gmail.com>
2025-08-01 13:15:14 +0530

    Update server.js
    Correct Setup of App

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
-app.use(express.static('public'));
+
 const app = express();
 const PORT = 3000;
 
+app.use(express.static('public'));
 app.use(cors());
 app.use(express.json());
 
